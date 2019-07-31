b214afc14a85a6b8518edb49969721e4ed363177
Jimmique <jimmique88@gmail.com>
2019-07-30 20:27:35 -0700

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Nasa from "./components/nasa.js";
 import "./App.css";
+import axios from "axios";
+import "./index.css";
 
 function App() {
+  const [nasa, setNasa] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=7TEedvatBtcZa95WqVIdCySPyv7Fed6GNGxpefyi")
+   .then((response) => {
+     setNasa(response.data)
+   }).catch((error) => {
+     console.log(error)
+   })
+ }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="shuttle">
+        <h1>NASA Photo of the Day</h1>
+         {console.log(nasa)}
+         {<Nasa date={nasa.date} explanation={nasa.explanation} 
+         src={nasa.url} media_type={nasa.media_type} hdurl={nasa.hdurl}
+         title={nasa.title}/> }
+      </div>
     </div>
   );
 }
